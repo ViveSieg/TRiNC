ca454aecb25eff0e32ff13a74903b3baa21f145a
ViveSieg <liyigethisway@gmail.com>
2025-11-15 10:39:49 +0800

    update

--- src/trinc/__init__.py
+++
@@ -1,5 +0,0 @@
-"""TRiNC thermal control benchmark package."""
-
-from .paths import ArtifactLayout, ensure_artifact_layout, get_artifact_layout
-
-__all__ = ["ArtifactLayout", "ensure_artifact_layout", "get_artifact_layout"]
